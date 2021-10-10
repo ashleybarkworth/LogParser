a1397c183d10f1b3b7b6f2b85123e7527b10a127
chandni <chandni@chandnis-MacBook-Pro.local>
2021-10-10 14:48:53 -0300

    update

--- affinityPropogation.py
+++ affinityPropogation.py
@@ -10,7 +10,7 @@ from sklearn.cluster import AffinityPropagation
 import distance
 
 def affinityPropogation():
-    df = pd.read_csv('/Users/chandni/BRICKS/SIEM/Windows_2k.csv')
+    df = pd.read_csv('/data/Windows_2k.log_structured.csv')
     words = df['Content'].tolist()
     words =  np.asarray(words) #So that indexing with a list will work
     lev_similarity = -1*np.array([[distance.levenshtein(w1,w2) for w1 in words] for w2 in words])
